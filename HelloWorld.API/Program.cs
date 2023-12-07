3d591c3a23aeb140f15bd00a0ab2a0558faef2dc
Mark Topping <marktopping@outlook.com>
2023-12-07 14:03:35 +0000

    Helloworld app

---
+++ HelloWorld.API/Program.cs
@@ -0,0 +1,32 @@
+namespace HelloWorld.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Starting Web App. Environment: {Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}");
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ HelloWorld.UnitTests/ExampleTests.cs
@@ -0,0 +1,15 @@
+namespace HelloWorld.UnitTests
+{
+    public class ExampleTests
+    {
+        [Fact]
+        public void ExampleTestOne() => Assert.Equal(1, 1);
+
+        [Fact]
+        public void ExampleTestTwo() => Assert.Equal(2, 2);
+
+        [Fact]
+        public void ExampleTestThree() => Assert.Equal(3, 3);
+
+    }
+}
