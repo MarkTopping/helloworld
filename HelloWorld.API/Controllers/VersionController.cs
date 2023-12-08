aed37bb2338cf9545e838519716ef0bf80307006
Mark Topping <marktopping@outlook.com>
2023-12-08 11:13:39 +0000

    added helm and a version endpoint

---
+++ HelloWorld.API/Controllers/VersionController.cs
@@ -0,0 +1,16 @@
+﻿using HelloWorld.API;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet(Name = "GetVersion")]
+        public string Get()
+        {
+            return $"Response from host: {Environment.GetEnvironmentVariable("HOSTNAME")}. Image version: {Environment.GetEnvironmentVariable("IMAGE_TAG")}";
+        }
+    }
+}
